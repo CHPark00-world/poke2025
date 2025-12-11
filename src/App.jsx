90926ba62c8e245512ccdde9642aa0ee07f32432
park <parkch2463@gmail.com>
2025-12-11 17:45:58 +0900

    로그인 페이지 UI 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing";
+import Login from "./pages/Login";
 
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
